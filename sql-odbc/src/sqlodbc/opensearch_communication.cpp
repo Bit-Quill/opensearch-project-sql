b7140bf02ca97cf31c271f33746ea804bc9ebb5d
Yury Fridlyand <yuryf@bitquilltech.com>
2022-02-23 09:59:07 -0800

    Added a comment for HTTP client release procedure
    Signed-off-by: Yury Fridlyand <yuryf@bitquilltech.com>

--- sql-odbc/src/sqlodbc/opensearch_communication.cpp
+++ sql-odbc/src/sqlodbc/opensearch_communication.cpp
@@ -243,6 +243,8 @@ OpenSearchCommunication::OpenSearchCommunication()
 }
 
 OpenSearchCommunication::~OpenSearchCommunication() {
+    // We should release HTTP client instanse to let it release its resources before releasing AWS SDK.
+    // Changing order of these actions would cause crash on disconnect.
     m_http_client.reset();
     --AWS_SDK_HELPER;
 }
