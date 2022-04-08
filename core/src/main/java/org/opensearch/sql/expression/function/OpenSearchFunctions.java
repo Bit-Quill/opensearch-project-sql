1ac04bdfb8e579d88fb4e4990e2764d741d74d85
Guian Gumpac <guiang@bitquilltech.com>
2022-04-08 06:56:39 -0700

    Added match_phrase to FunctionResolver and modified test
    Signed-off-by: Guian Gumpac <guiang@bitquilltech.com>

--- core/src/main/java/org/opensearch/sql/expression/function/OpenSearchFunctions.java
+++ core/src/main/java/org/opensearch/sql/expression/function/OpenSearchFunctions.java
@@ -25,6 +25,7 @@ import org.opensearch.sql.expression.env.Environment;
 public class OpenSearchFunctions {
   public void register(BuiltinFunctionRepository repository) {
     repository.register(match());
+    repository.register(match_phrase());
   }
 
   private static FunctionResolver match() {
@@ -71,6 +72,19 @@ public class OpenSearchFunctions {
             .put(new FunctionSignature(funcName, ImmutableList
                     .of(STRING, STRING, STRING, STRING, STRING, STRING, STRING, STRING, STRING,
                         STRING, STRING, STRING, STRING, STRING)),
+                args -> new OpenSearchFunction(funcName, args))
+            .build());
+  }
+
+  private static FunctionResolver match_phrase() {
+    FunctionName funcName = BuiltinFunctionName.MATCH_PHRASE.getName();
+    return new FunctionResolver(funcName,
+        ImmutableMap.<FunctionSignature, FunctionBuilder>builder()
+            .put(new FunctionSignature(funcName, ImmutableList.of(STRING, STRING)),
+                args -> new OpenSearchFunction(funcName, args))
+            .put(new FunctionSignature(funcName, ImmutableList.of(STRING, STRING, STRING)),
+                args -> new OpenSearchFunction(funcName, args))
+            .put(new FunctionSignature(funcName, ImmutableList.of(STRING, STRING, STRING, STRING)),
                 args -> new OpenSearchFunction(funcName, args))
             .build());
   }
