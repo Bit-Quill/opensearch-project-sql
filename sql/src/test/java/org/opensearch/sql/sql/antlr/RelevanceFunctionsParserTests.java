d9617ae7436cc17e85d5f42b216ebcf6133b0205
MaxKsyunz <maxk@bitquilltech.com>
2022-05-13 21:31:46 -0700

    Re-arrange unit tests to highlight what's important.
    Signed-off-by: MaxKsyunz <maxk@bitquilltech.com>

--- sql/src/test/java/org/opensearch/sql/sql/antlr/RelevanceFunctionsParserTests.java
+++ sql/src/test/java/org/opensearch/sql/sql/antlr/RelevanceFunctionsParserTests.java
@@ -1,37 +1,38 @@
 package org.opensearch.sql.sql.antlr;
 
-import java.util.stream.Stream;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 public class RelevanceFunctionsParserTests extends SQLParserTestBase {
 
-  @ParameterizedTest
-  @MethodSource("simple_query_string_validQueries")
-  public void testValidQuery(String query) {
-    assertAccepted(query);
+  @Test
+  void acceptDoubleQuoteStar() {
+    assertAccepted("SELECT * FROM T WHERE simple_query_string([\"*\"], \"sample\");");
+  }
+  @Test
+  void acceptSingleQuoteStar() {
+    assertAccepted("SELECT * FROM T WHERE simple_query_string(['*'], \"sample\");");
+  }
+  @Test
+  void rejectMultipleStars() {
+    assertRejected("SELECT * FROM T WHERE simple_query_string(['*', '*'], 'asdf');");
   }
 
-
-  @ParameterizedTest
-  @MethodSource("simple_query_string_invalidQueries")
-  public void testInvalidQuery(String query) {
-    assertRejected(query);
+  @Test
+  void rejectUnquotedStar() {
+    assertRejected("SELECT * FROM T WHERE simple_query_string([*], 'asdf');");
+  }
+  @Test
+  void rejectAllFields() {
+    assertRejected("SELECT * FROM T WHERE simple_query_string(*, 'asdf');");
   }
 
-  private static Stream<String> simple_query_string_invalidQueries() {
-    return Stream.of(
-        "SELECT * FROM T WHERE simple_query_string();",
-        "SELECT * FROM T WHERE simple_query_string([], 'asdf');",
-        "SELECT * FROM T WHERE simple_query_string(*, 'asdf');",
-        "SELECT * FROM T WHERE simple_query_string([*], 'asdf');"
-    );
+  @Test
+  void rejectEmptyFieldList() {
+    assertRejected("SELECT * FROM T WHERE simple_query_string([], 'asdf');");
   }
 
-  private static Stream<String> simple_query_string_validQueries() {
-    return Stream.of(
-        "SELECT * FROM T WHERE simple_query_string(['*'], \"sample\");",
-        "SELECT * FROM T WHERE simple_query_string([\"*\"], \"sample\");"
-    );
+  @Test
+  void rejectNoArguments() {
+    assertRejected("SELECT * FROM T WHERE simple_query_string();");
   }
 }
