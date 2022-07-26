efe0f39cda12a1f32ff42db75c30df81f56ce008
forestmvey <forestv@bitquilltech.com>
2022-07-26 08:03:19 -0700

    Removed OpenSearchLogicalIndexScan highlightFields and dependencies. Improved test coverage and fixing checkstyle errors.
    Signed-off-by: forestmvey <forestv@bitquilltech.com>

--- core/src/main/java/org/opensearch/sql/expression/function/OpenSearchFunctions.java
+++ core/src/main/java/org/opensearch/sql/expression/function/OpenSearchFunctions.java
@@ -24,7 +24,6 @@ import org.opensearch.sql.expression.FunctionExpression;
 import org.opensearch.sql.expression.HighlightExpression;
 import org.opensearch.sql.expression.NamedArgumentExpression;
 import org.opensearch.sql.expression.env.Environment;
-import org.opensearch.sql.common.utils.StringUtils;
 
 @UtilityClass
 public class OpenSearchFunctions {
@@ -58,7 +57,6 @@ public class OpenSearchFunctions {
   private static FunctionResolver highlight() {
     FunctionName functionName = BuiltinFunctionName.HIGHLIGHT.getName();
     FunctionSignature functionSignature = new FunctionSignature(functionName, List.of(STRING));
-    // TODO change to pass the argument as a StringLiteral
     FunctionBuilder functionBuilder = arguments -> new HighlightExpression(arguments.get(0));
     return new FunctionResolver(functionName, ImmutableMap.of(functionSignature, functionBuilder));
   }
