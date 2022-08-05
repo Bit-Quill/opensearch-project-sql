1e92dbeb0fbbec28a9f5c50cda41c314f5cf69ba
mitchellg <mitchellg@bitquilltech.com>
2022-08-05 10:37:57 -0700

    Merged from upstream
    Signed-off-by: mitchellg <mitchellg@bitquilltech.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/relevance/SimpleQueryStringQuery.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/relevance/SimpleQueryStringQuery.java
@@ -27,12 +27,11 @@ public class SimpleQueryStringQuery extends RelevanceQuery<SimpleQueryStringBuil
   public SimpleQueryStringQuery() {
     super(ImmutableMap.<String, QueryBuilderStep<SimpleQueryStringBuilder>>builder()
         .put("analyze_wildcard", (b, v) -> b.analyzeWildcard(Boolean.parseBoolean(v.stringValue())))
-        .put("analyzer", (b, v) -> b.analyzer(valueOfToUpper(v)))
+        .put("analyzer", (b, v) -> b.analyzer(v.stringValue()))
         .put("auto_generate_synonyms_phrase_query", (b, v) ->
             b.autoGenerateSynonymsPhraseQuery(Boolean.parseBoolean(v.stringValue())))
         .put("boost", (b, v) -> b.boost(Float.parseFloat(v.stringValue())))
-        .put("default_operator", (b, v) ->
-            b.defaultOperator(Operator.fromString(v.stringValue())))
+        .put("default_operator", (b, v) -> b.defaultOperator(Operator.fromString(valueOfToUpper(v))))
         .put("flags", (b, v) ->
             b.flags(Arrays.stream((v.stringValue().toUpperCase()).split("\\|"))
             .map(SimpleQueryStringFlag::valueOf)
