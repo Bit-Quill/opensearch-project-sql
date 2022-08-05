f54fad4c80cb8a2553c1fa06d1f2eed22e924aa6
mitchellg <mitchellg@bitquilltech.com>
2022-08-05 09:39:34 -0700

    Merged from upstream
    Signed-off-by: mitchellg <mitchellg@bitquilltech.com>

--- opensearch/src/test/java/org/opensearch/sql/opensearch/storage/script/filter/FilterQueryBuilderTest.java
+++ opensearch/src/test/java/org/opensearch/sql/opensearch/storage/script/filter/FilterQueryBuilderTest.java
@@ -851,7 +851,8 @@ class FilterQueryBuilderTest {
         dsl.namedArgument("query", literal("search query")),
         dsl.namedArgument("zero_terms_query", literal("meow")));
     var msg = assertThrows(IllegalArgumentException.class, () -> buildQuery(expr)).getMessage();
-    assertEquals("No enum constant org.opensearch.index.search.MatchQuery.ZeroTermsQuery.meow",
+    assertEquals("No enum constant org.opensearch.index.search.MatchQuery.ZeroTermsQuery.meow" +
+            "",
           msg);
   }
 
