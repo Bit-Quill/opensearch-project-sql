d5e8a2180a259758f9f44a1d598077c4d9f14b0c
Yury-Fridlyand <yuryf@bitquilltech.com>
2022-08-12 11:38:41 -0700

    Add implementation of `now`, `sysdate`, `localtime` and similar functions (#92)
    Signed-off-by: Yury-Fridlyand <yuryf@bitquilltech.com>

--- legacy/src/main/java/org/opensearch/sql/legacy/plugin/RestSqlAction.java
+++ legacy/src/main/java/org/opensearch/sql/legacy/plugin/RestSqlAction.java
@@ -125,6 +125,7 @@ public class RestSqlAction extends BaseRestHandler {
         Metrics.getInstance().getNumericalMetric(MetricName.REQ_COUNT_TOTAL).increment();
 
         QueryContext.addRequestId();
+        QueryContext.recordProcessingStarted();
 
         try {
             if (!isSQLFeatureEnabled()) {
