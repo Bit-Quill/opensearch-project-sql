7b9e6d1903be06c25777b16d8d6dcfaf1e3fa571
forestmvey <forestv@bitquilltech.com>
2022-09-20 14:33:01 -0700

    Update MacOS version to supported level. Updated aws-sdk-cpp dependency and googletest to resolve compilation errors.

--- sql-odbc/aws_sdk_cpp_setup.sh
+++ sql-odbc/aws_sdk_cpp_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd src
-git clone -b "1.7.329" "https://github.com/aws/aws-sdk-cpp.git"
+git clone -b "1.8.0" "https://github.com/aws/aws-sdk-cpp.git"
 cd ..
--- sql-odbc/build_mac_debug64.sh
+++ sql-odbc/build_mac_debug64.sh
@@ -2,7 +2,7 @@
 # $BITNESS=64
 
 cd src
-git clone -b "1.7.329" "https://github.com/aws/aws-sdk-cpp.git"
+git clone -b "1.8.0" "https://github.com/aws/aws-sdk-cpp.git"
 cd ..
 
 PREFIX_PATH=$(pwd)
