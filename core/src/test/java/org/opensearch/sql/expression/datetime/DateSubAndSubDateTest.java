6e7c86bb45dcdad789d06733503e26318fa4fe1c
Yury-Fridlyand <yuryf@bitquilltech.com>
2022-10-11 15:12:06 -0700

    Split UT cases into smaller ones.
    Signed-off-by: Yury-Fridlyand <yuryf@bitquilltech.com>

--- core/src/test/java/org/opensearch/sql/expression/datetime/DateSubAndSubDateTest.java
+++ core/src/test/java/org/opensearch/sql/expression/datetime/DateSubAndSubDateTest.java
@@ -23,77 +23,95 @@ import org.opensearch.sql.exception.ExpressionEvaluationException;
 public class DateSubAndSubDateTest extends DateTimeTestBase {
 
   @Test
-  public void returns_datetime_when_args_are_time_and_time_interval() {
+  public void subdate_returns_datetime_when_args_are_time_and_time_interval() {
     var res = subdate(LocalTime.of(21, 0), Duration.ofHours(1).plusMinutes(2));
     assertEquals(DATETIME, res.type());
     assertEquals(LocalTime.of(19, 58).atDate(LocalDate.now()), res.datetimeValue());
+  }
 
-    res = date_sub(LocalTime.of(10, 20, 30),
+  @Test
+  public void date_sub_returns_datetime_when_args_are_time_and_time_interval() {
+    var res = date_sub(LocalTime.of(10, 20, 30),
         Duration.ofHours(1).plusMinutes(2).plusSeconds(42));
     assertEquals(DATETIME, res.type());
     assertEquals(LocalTime.of(9, 17, 48).atDate(LocalDate.now()), res.datetimeValue());
   }
 
   @Test
-  public void time_limited_by_24_hours() {
+  public void subdate_time_limited_by_24_hours() {
     var res = subdate(LocalTime.MIN, Duration.ofNanos(1));
     assertEquals(DATETIME, res.type());
     assertEquals(LocalTime.MAX, res.datetimeValue().toLocalTime());
+  }
 
-    res = date_sub(LocalTime.of(10, 20, 30),
+  @Test
+  public void date_sub_time_limited_by_24_hours() {
+    var res = date_sub(LocalTime.of(10, 20, 30),
         Duration.ofHours(20).plusMinutes(50).plusSeconds(7));
     assertEquals(DATETIME, res.type());
     assertEquals(LocalTime.of(13, 30, 23), res.datetimeValue().toLocalTime());
   }
 
   @Test
-  public void returns_datetime_when_args_are_date_and_date_interval() {
+  public void subdate_returns_datetime_when_args_are_date_and_date_interval() {
     var res = subdate(LocalDate.of(2020, 2, 20), Period.of(3, 11, 21));
     assertEquals(DATETIME, res.type());
     assertEquals(LocalDate.of(2016, 2, 28).atStartOfDay(), res.datetimeValue());
+  }
 
-    res = date_sub(LocalDate.of(1961, 4, 12), Period.of(50, 50, 50));
+  @Test
+  public void date_sub_returns_datetime_when_args_are_date_and_date_interval() {
+    var res = date_sub(LocalDate.of(1961, 4, 12), Period.of(50, 50, 50));
     assertEquals(DATETIME, res.type());
     assertEquals(LocalDate.of(1906, 12, 24).atStartOfDay(), res.datetimeValue());
   }
 
   @Test
-  public void returns_datetime_when_args_are_date_and_time_interval() {
+  public void subdate_returns_datetime_when_args_are_date_and_time_interval() {
     var res = subdate(LocalDate.of(2020, 2, 20), Duration.ofHours(1).plusMinutes(2));
     assertEquals(DATETIME, res.type());
     assertEquals(LocalDateTime.of(2020, 2, 19, 22, 58), res.datetimeValue());
+  }
 
-    res = date_sub(LocalDate.of(1961, 4, 12), Duration.ofHours(9).plusMinutes(7));
+  @Test
+  public void date_sub_returns_datetime_when_args_are_date_and_time_interval() {
+    var res = date_sub(LocalDate.of(1961, 4, 12), Duration.ofHours(9).plusMinutes(7));
     assertEquals(DATETIME, res.type());
     assertEquals(LocalDateTime.of(1961, 4, 11, 14, 53), res.datetimeValue());
   }
 
   @Test
-  public void returns_datetime_when_args_are_time_and_date_interval() {
+  public void subdate_returns_datetime_when_args_are_time_and_date_interval() {
     // Date based on today
     var res = subdate(LocalTime.of(1, 2, 0), Period.ofDays(1));
     assertEquals(DATETIME, res.type());
     assertEquals(LocalDate.now().minusDays(1).atTime(LocalTime.of(1, 2, 0)), res.datetimeValue());
+  }
 
-    res = date_sub(LocalTime.MIDNIGHT, Period.ofDays(0));
+  @Test
+  public void date_sub_returns_datetime_when_args_are_time_and_date_interval() {
+    var res = date_sub(LocalTime.MIDNIGHT, Period.ofDays(0));
     assertEquals(DATETIME, res.type());
     assertEquals(LocalDate.now().atStartOfDay(), res.datetimeValue());
   }
 
   @Test
-  public void returns_datetime_when_first_arg_is_datetime_or_timestamp() {
+  public void subdate_returns_datetime_when_first_arg_is_datetime_or_timestamp() {
     var res = subdate(LocalDateTime.of(1961, 4, 12, 9, 7), Duration.ofMinutes(108));
     assertEquals(DATETIME, res.type());
     assertEquals(LocalDateTime.of(1961, 4, 12, 7, 19), res.datetimeValue());
+  }
 
-    res = date_sub(LocalDateTime.of(1961, 4, 12, 9, 7).toInstant(ZoneOffset.UTC),
+  @Test
+  public void date_sub_returns_datetime_when_first_arg_is_datetime_or_timestamp() {
+    var res = date_sub(LocalDateTime.of(1961, 4, 12, 9, 7).toInstant(ZoneOffset.UTC),
         Duration.ofMinutes(108));
     assertEquals(DATETIME, res.type());
     assertEquals(LocalDateTime.of(1961, 4, 12, 7, 19), res.datetimeValue());
   }
 
   @Test
-  public void accepts_negative_interval() {
+  public void subdate_accepts_negative_interval() {
     var res = subdate(LocalDateTime.of(2020, 10, 20, 14, 42), Duration.ofDays(-10));
     assertEquals(DATETIME, res.type());
     assertEquals(LocalDateTime.of(2020, 10, 30, 14, 42), res.datetimeValue());
