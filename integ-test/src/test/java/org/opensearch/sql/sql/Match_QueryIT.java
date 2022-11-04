d1ea1ca9fe2f3f638d69684fffd74517e8c1f91f
GabeFernandez310 <gabrielf@bitquilltech.com>
2022-11-04 09:36:01 -0700

    Added Integration Tests for MatchQuery and Match_Query Functions
    Signed-off-by: GabeFernandez310 <gabrielf@bitquilltech.com>

--- integ-test/src/test/java/org/opensearch/sql/sql/Match_QueryIT.java
+++ integ-test/src/test/java/org/opensearch/sql/sql/Match_QueryIT.java
@@ -17,22 +17,22 @@ import static org.opensearch.sql.util.MatcherUtils.schema;
 import static org.opensearch.sql.util.MatcherUtils.verifyDataRows;
 import static org.opensearch.sql.util.MatcherUtils.verifySchema;
 
-public class MatchQueryIT extends SQLIntegTestCase {
+public class Match_QueryIT extends SQLIntegTestCase {
   @Override
   public void init() throws IOException {
     loadIndex(Index.ACCOUNT);
   }
 
   @Test
-  public void matchquery_in_where() throws IOException {
-    JSONObject result = executeJdbcRequest("SELECT firstname FROM " + TEST_INDEX_ACCOUNT + " WHERE matchquery(lastname, 'Bates')");
+  public void match_query_in_where() throws IOException {
+    JSONObject result = executeJdbcRequest("SELECT firstname FROM " + TEST_INDEX_ACCOUNT + " WHERE match_query(lastname, 'Bates')");
     verifySchema(result, schema("firstname", "text"));
     verifyDataRows(result, rows("Nanette"));
   }
 
   @Test
-  public void matchquery_in_having() throws IOException {
-    JSONObject result = executeJdbcRequest("SELECT lastname FROM " + TEST_INDEX_ACCOUNT + " HAVING matchquery(firstname, 'Nanette')");
+  public void match_query_in_having() throws IOException {
+    JSONObject result = executeJdbcRequest("SELECT lastname FROM " + TEST_INDEX_ACCOUNT + " HAVING match_query(firstname, 'Nanette')");
     verifySchema(result, schema("lastname", "text"));
     verifyDataRows(result, rows("Bates"));
   }
