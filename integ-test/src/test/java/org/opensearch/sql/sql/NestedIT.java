4c22ced59f5e784cad326746544384acfb44f0e7
Guian Gumpac <guiang@bitquilltech.com>
2022-12-06 08:41:37 -0800

    Added multilevel nested test data
    Signed-off-by: Guian Gumpac <guiang@bitquilltech.com>

--- integ-test/src/test/java/org/opensearch/sql/sql/NestedIT.java
+++ integ-test/src/test/java/org/opensearch/sql/sql/NestedIT.java
@@ -11,17 +11,17 @@ import org.opensearch.sql.legacy.SQLIntegTestCase;
 
 import java.io.IOException;
 
-import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_NESTED_TYPE;
+import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_MULTI_NESTED;
 
 public class NestedIT extends SQLIntegTestCase {
   @Override
   public void init() throws IOException {
-    loadIndex(Index.NESTED);
+    loadIndex(Index.MULTI_NESTED);
   }
 
   @Test
   public void nested_string_subfield_test() {
-    String query = "SELECT nested(message.info) FROM " + TEST_INDEX_NESTED_TYPE;
+    String query = "SELECT nested(message.info) FROM " + TEST_INDEX_MULTI_NESTED;
     JSONObject result = executeJdbcRequest(query);
     assertEquals(6, result.getInt("total"));
   }
