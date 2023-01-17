18e7e21e560bace80cd99410500a8f65a9a6c789
Yury-Fridlyand <yury.fridlyand@improving.com>
2023-01-17 09:32:24 -0800

    Fix compilation warnings for `EqualsAndHashCode`.
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/executor/protector/ResourceMonitorPlan.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/executor/protector/ResourceMonitorPlan.java
@@ -20,7 +20,7 @@ import org.opensearch.sql.planner.physical.PhysicalPlanNodeVisitor;
  */
 @ToString
 @RequiredArgsConstructor
-@EqualsAndHashCode(callSuper = true)
+@EqualsAndHashCode(callSuper = false)
 public class ResourceMonitorPlan extends PhysicalPlan {
 
   /**
