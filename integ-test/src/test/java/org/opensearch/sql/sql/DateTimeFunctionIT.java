55f394e2678b092942050ad2db9ec25e2e0d6df9
GabeFernandez310 <Gabriel.Fernandez@improving.com>
2023-02-06 12:15:23 -0800

    Added Integration Test
    Signed-off-by: GabeFernandez310 <Gabriel.Fernandez@improving.com>

--- integ-test/src/test/java/org/opensearch/sql/sql/DateTimeFunctionIT.java
+++ integ-test/src/test/java/org/opensearch/sql/sql/DateTimeFunctionIT.java
@@ -443,6 +443,16 @@ public class DateTimeFunctionIT extends SQLIntegTestCase {
   }
 
   @Test
+  public void testDayLastDay() throws IOException {
+    JSONObject result = executeQuery(
+        String.format("SELECT last_day(CAST(date0 AS date)) FROM %s LIMIT 3", TEST_INDEX_CALCS));
+    verifyDataRows(result,
+        rows("2004-04-30"),
+        rows("1972-07-31"),
+        rows("1975-11-30"));
+  }
+
+  @Test
   public void testMicrosecond() throws IOException {
     JSONObject result = executeQuery("select microsecond(timestamp('2020-09-16 17:30:00.123456'))");
     verifySchema(result,
