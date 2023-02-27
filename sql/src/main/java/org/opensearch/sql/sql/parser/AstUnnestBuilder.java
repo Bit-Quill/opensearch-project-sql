e0b4f9480d230d8c491587a4d8cb31cd26dce69e
forestmvey <forestv@bitquilltech.com>
2023-02-27 13:04:53 -0800

    Fix Alias name for nested function. Re-add support for returning arrays for reference expressions.
    Signed-off-by: forestmvey <forestv@bitquilltech.com>

--- sql/src/main/java/org/opensearch/sql/sql/parser/AstUnnestBuilder.java
+++ sql/src/main/java/org/opensearch/sql/sql/parser/AstUnnestBuilder.java
@@ -7,6 +7,7 @@ package org.opensearch.sql.sql.parser;
 
 import lombok.RequiredArgsConstructor;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.opensearch.sql.ast.expression.Alias;
 import org.opensearch.sql.ast.expression.Function;
 import org.opensearch.sql.ast.expression.UnresolvedExpression;
 import org.opensearch.sql.ast.tree.Unnest;
@@ -26,11 +27,13 @@ public class AstUnnestBuilder extends OpenSearchSQLParserBaseVisitor<UnresolvedP
   public UnresolvedPlan visit(ParseTree selectClause) {
     Unnest unnest = null;
     for (UnresolvedExpression item : querySpec.getSelectItems()) {
-      if (item instanceof Function && ((Function)item).getFuncName().equalsIgnoreCase("nested")) {
+      if (item instanceof Alias
+          && ((Alias)item).getDelegated() instanceof Function
+          && ((Function)((Alias)item).getDelegated()).getFuncName().equalsIgnoreCase("nested")) {
         if (unnest == null) {
-          unnest = buildUnnest((Function)item);
+          unnest = buildUnnest((Function)((Alias)item).getDelegated());
         } else {
-          unnest.add((Function)item);
+          unnest.add((Function)((Alias)item).getDelegated());
         }
       }
     }
