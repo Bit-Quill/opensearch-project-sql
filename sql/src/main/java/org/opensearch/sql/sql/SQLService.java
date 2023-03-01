d8828301bcf0741888dc7b9159061770fbd7cb49
Guian Gumpac <guian.gumpac@improving.com>
2023-03-01 14:03:01 -0800

    Refactored fall back logic to use visitor design pattern
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>

--- sql/src/main/java/org/opensearch/sql/sql/SQLService.java
+++ sql/src/main/java/org/opensearch/sql/sql/SQLService.java
@@ -6,10 +6,15 @@
 
 package org.opensearch.sql.sql;
 
-import java.util.List;
 import java.util.Optional;
+
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.apache.commons.lang3.StringUtils;
+import org.opensearch.sql.ast.AbstractNodeVisitor;
+import org.opensearch.sql.ast.Node;
 import org.opensearch.sql.ast.expression.Alias;
 import org.opensearch.sql.ast.expression.Cast;
 import org.opensearch.sql.ast.expression.Function;
@@ -17,8 +22,6 @@ import org.opensearch.sql.ast.expression.Literal;
 import org.opensearch.sql.ast.statement.Query;
 import org.opensearch.sql.ast.statement.Statement;
 import org.opensearch.sql.ast.tree.Aggregation;
-import org.opensearch.sql.ast.tree.Filter;
-import org.opensearch.sql.ast.tree.Limit;
 import org.opensearch.sql.ast.tree.Project;
 import org.opensearch.sql.common.response.ResponseListener;
 import org.opensearch.sql.executor.ExecutionEngine.ExplainResponse;
@@ -75,9 +78,7 @@ public class SQLService {
       SQLQueryRequest request,
       Optional<ResponseListener<QueryResponse>> queryListener,
       Optional<ResponseListener<ExplainResponse>> explainListener) {
-    // Hints are currently unsupported for V2
-    ParseTree cstHints = parser.parseHints(request.getQuery());
-    if (cstHints.getChildCount() > 1)
+    if (parser.containsHints(request.getQuery()))
       throw new UnsupportedOperationException("Hints are not yet supported in the new engine.");
 
     // 1.Parse query and convert parse tree (CST) to abstract syntax tree (AST)
@@ -93,39 +94,88 @@ public class SQLService {
     // There is no full support for JSON format yet for in memory operations, aliases, literals, and casts
     // Aggregation has differences with legacy results
     if (request.format().getFormatName().equals("json")) {
-      List projectList = ((Project) ((Query) statement).getPlan()).getProjectList();
-      List planChildren = ((Project) ((Query) statement).getPlan()).getChild();
-      UnsupportedOperationException aggException = new UnsupportedOperationException(
-          "Queries with aggregation are not yet supported with json format in the new engine");
+      class VisitorContext {
+        @Getter
+        @Setter
+        boolean isJSONSupported = true;
+      }
 
-      for (var child : planChildren) {
-        if (child instanceof Aggregation && ((Aggregation) child).getGroupExprList().size() > 0)
-          throw aggException;
-
-        if (child instanceof Filter) {
-          for (var filterChild : ((Filter) child).getChild())
-            if (filterChild instanceof Aggregation)
-              throw aggException;
+      class JsonSupportedVisitor extends AbstractNodeVisitor<Boolean, VisitorContext> {
+        @Override
+        public Boolean visit(Node node, VisitorContext context) {
+          // A node is supported if it's a leaf node or if all of its children are supported.
+          return node.getChild().isEmpty()
+              || node.getChild().stream().filter(c -> c.accept(this, context) != null)
+              .allMatch(c -> c.accept(this, context));
         }
 
-        if (child instanceof Limit) {
-          for (var filterChild : ((Limit) child).getChild())
-            if(filterChild instanceof Aggregation)
-              throw aggException;
+        @Override
+        public Boolean visitAlias(Alias node, VisitorContext context) {
+          // Alias node is accepted if it does not have a user-defined alias
+          // and if the delegated expression is accepted.
+          if (!StringUtils.isEmpty(node.getAlias()))
+            return false;
+          else
+            return node.getDelegated().accept(this, context);
+        }
+
+        @Override
+        public Boolean visitProject(Project node, VisitorContext context) {
+          // Overridden visit functions are done in memory and are not supported with json format
+          class UnsupportedExpressionsVisitor
+              extends AbstractNodeVisitor<Boolean, Object> {
+
+            @Override
+            public Boolean visitFunction(Function node, Object context) {
+              // queries with function calls are not supported.
+              return false;
+            }
+
+            @Override
+            public Boolean visitLiteral(Literal node, Object context) {
+              // queries with literal values are not supported
+              return false;
+            }
+
+            @Override
+            public Boolean visitCast(Cast node, Object context) {
+              // Queries with cast are not supported
+              return false;
+            }
+
+            @Override
+            public Boolean visitAlias(Alias node, Object context) {
+              // Alias node is accepted if it does not have a user-defined alias
+              // and if the delegated expression is accepted.
+              if (!StringUtils.isEmpty(node.getAlias()))// && node.getDelegated().accept(this, context))
+                return false;
+              else {
+                return node.getDelegated().accept(this, context);
+              }
+            }
+          }
+
+          // Project node is supported if all of its children are supported and
+          // all expressions in project list are supported.
+          UnsupportedExpressionsVisitor unsupportedExpressionsVisitor = new UnsupportedExpressionsVisitor();
+          return visit(node, context)
+              && node.getProjectList().stream().filter(c -> c.accept(unsupportedExpressionsVisitor, context) != null)
+              .allMatch(e -> e.accept(unsupportedExpressionsVisitor, context));
+
+        }
+
+        @Override
+        public Boolean visitAggregation(Aggregation node, VisitorContext context) {
+          return node.getGroupExprList().isEmpty();
         }
       }
 
-      for (var project : projectList) {
-        if (project instanceof Alias) {
-          Alias projectAlias = ((Alias) project);
+      Boolean isJsonSupported = ((Query) statement).getPlan().accept(new JsonSupportedVisitor(), new VisitorContext());
 
-          if (projectAlias.getDelegated() instanceof Function ||
-              projectAlias.getDelegated() instanceof Literal ||
-              projectAlias.getDelegated() instanceof Cast ||
-              (projectAlias.getAlias() != null && !projectAlias.getAlias().isEmpty()))
-            throw new UnsupportedOperationException("The query is not yet supported with json "
-                + "format because there is an expression in the query that is not pushed down to the OpenSearch instance");
-        }
+      if (!isJsonSupported) {
+        throw new UnsupportedOperationException(
+            "Queries with aggregation and in memory operations (cast, literals, alias, math, etc.)"
+                + " are not yet supported with json format in the new engine");
       }
     }
 
