ce02e9dc4e26b8069c5121a21e42b331a581d1fa
Guian Gumpac <guian.gumpac@improving.com>
2023-03-02 15:09:58 -0800

    Addressed PR feedback
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>

--- sql/src/main/java/org/opensearch/sql/sql/SQLService.java
+++ sql/src/main/java/org/opensearch/sql/sql/SQLService.java
@@ -13,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.apache.commons.lang3.StringUtils;
+import org.opensearch.sql.analysis.JsonSupportAnalysisContext;
+import org.opensearch.sql.analysis.JsonSupportAnalyzer;
 import org.opensearch.sql.ast.AbstractNodeVisitor;
 import org.opensearch.sql.ast.Node;
 import org.opensearch.sql.ast.expression.Alias;
@@ -94,72 +96,8 @@ public class SQLService {
     // There is no full support for JSON format yet for in memory operations, aliases, literals, and casts
     // Aggregation has differences with legacy results
     if (request.format().getFormatName().equals("json")) {
-      class VisitorContext {
-        @Getter
-        @Setter
-        boolean isJSONSupported = true;
-      }
-
-      class JsonSupportedVisitor extends AbstractNodeVisitor<Boolean, VisitorContext> {
-        @Override
-        public Boolean visit(Node node, VisitorContext context) {
-          // A node is supported if all of its children are supported.
-          return node.getChild().stream().filter(c -> c.accept(this, context) != null)
-              .allMatch(c -> c.accept(this, context));
-        }
-
-        @Override
-        public Boolean visitProject(Project node, VisitorContext context) {
-          // Overridden visit functions are done in memory and are not supported with json format
-          class UnsupportedExpressionsVisitor
-              extends AbstractNodeVisitor<Boolean, Object> {
-
-            @Override
-            public Boolean visitFunction(Function node, Object context) {
-              // queries with function calls are not supported.
-              return false;
-            }
-
-            @Override
-            public Boolean visitLiteral(Literal node, Object context) {
-              // queries with literal values are not supported
-              return false;
-            }
-
-            @Override
-            public Boolean visitCast(Cast node, Object context) {
-              // Queries with cast are not supported
-              return false;
-            }
-
-            @Override
-            public Boolean visitAlias(Alias node, Object context) {
-              // Alias node is accepted if it does not have a user-defined alias
-              // and if the delegated expression is accepted.
-              if (!StringUtils.isEmpty(node.getAlias()))// && node.getDelegated().accept(this, context))
-                return false;
-              else {
-                return node.getDelegated().accept(this, context);
-              }
-            }
-          }
-
-          // Project node is supported if all of its children are supported and
-          // all expressions in project list are supported.
-          UnsupportedExpressionsVisitor unsupportedExpressionsVisitor = new UnsupportedExpressionsVisitor();
-          return visit(node, context)
-              && node.getProjectList().stream().filter(c -> c.accept(unsupportedExpressionsVisitor, context) != null)
-              .allMatch(e -> e.accept(unsupportedExpressionsVisitor, context));
-
-        }
-
-        @Override
-        public Boolean visitAggregation(Aggregation node, VisitorContext context) {
-          return node.getGroupExprList().isEmpty();
-        }
-      }
-
-      Boolean isJsonSupported = ((Query) statement).getPlan().accept(new JsonSupportedVisitor(), new VisitorContext());
+      Boolean isJsonSupported = ((Query) statement).getPlan()
+          .accept(new JsonSupportAnalyzer(), new JsonSupportAnalysisContext());
 
       if (!isJsonSupported) {
         throw new UnsupportedOperationException(
