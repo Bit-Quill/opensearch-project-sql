6e5164782ee4eaebd68a477cff7d6c763f0ad502
GabeFernandez310 <Gabriel.Fernandez@improving.com>
2023-03-14 14:46:22 -0700

    Added More Unit Tests And IT Tests
    Signed-off-by: GabeFernandez310 <Gabriel.Fernandez@improving.com>

--- core/src/test/java/org/opensearch/sql/expression/datetime/TimeStampAddTest.java
+++ core/src/test/java/org/opensearch/sql/expression/datetime/TimeStampAddTest.java
@@ -6,7 +6,9 @@
 
 package org.opensearch.sql.expression.datetime;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -23,6 +25,7 @@ import org.opensearch.sql.data.model.ExprStringValue;
 import org.opensearch.sql.data.model.ExprTimeValue;
 import org.opensearch.sql.data.model.ExprTimestampValue;
 import org.opensearch.sql.data.model.ExprValue;
+import org.opensearch.sql.exception.SemanticCheckException;
 import org.opensearch.sql.expression.DSL;
 import org.opensearch.sql.expression.Expression;
 import org.opensearch.sql.expression.ExpressionTestBase;
@@ -32,7 +35,6 @@ class TimeStampAddTest extends ExpressionTestBase {
 
   private static Stream<Arguments> getTestDataForTimestampAdd() {
     return Stream.of(
-        //TODO: Add more test cases
         Arguments.of("MINUTE", 1, new ExprStringValue("2003-01-02"),
             "2003-01-02 00:01:00"),
         Arguments.of("WEEK", 1, new ExprStringValue("2003-01-02"),
@@ -53,10 +55,10 @@ class TimeStampAddTest extends ExpressionTestBase {
         Arguments.of("WEEK", 1, new ExprTimestampValue("2003-01-02 00:00:00"),
             "2003-01-09"),
         //time
+        //Note: Adding date part to time argument requires function properties and is handled in another test.
         Arguments.of("MINUTE", 1, new ExprTimestampValue("10:11:12"),
             "10:12:12"),
-        //Note: Adding date part to time argument requires function properties and is handled in another test.
-        //TODO: Add cases surrounding leap year
+        //Cases surrounding leap year
         Arguments.of("SECOND", 1, new ExprTimestampValue("2020-02-28 23:59:59"),
             "2020-02-29 00:00:00"),
         Arguments.of("MINUTE", 1, new ExprTimestampValue("2020-02-28 23:59:59"),
@@ -67,29 +69,32 @@ class TimeStampAddTest extends ExpressionTestBase {
             "2020-02-29 23:59:59"),
         Arguments.of("WEEK", 1, new ExprTimestampValue("2020-02-28 23:59:59"),
             "2020-03-06 23:59:59"),
-        //TODO: Add cases surround end-of-year
+        //Cases surrounding end-of-year
         Arguments.of("SECOND", 1, new ExprTimestampValue("2020-12-31 23:59:59"),
-            "10:12:12"),
+            "2021-01-01 00:00:00"),
         Arguments.of("MINUTE", 1, new ExprTimestampValue("2020-12-31 23:59:59"),
-            "10:12:12"),
+            "2021-01-01 00:00:59"),
         Arguments.of("HOUR", 1, new ExprTimestampValue("2020-12-31 23:59:59"),
-            "10:12:12"),
+            "2021-01-01 00:59:59"),
         Arguments.of("DAY", 1, new ExprTimestampValue("2020-12-31 23:59:59"),
-            "10:12:12"),
+            "2021-01-01 23:59:59"),
         Arguments.of("WEEK", 1, new ExprTimestampValue("2020-12-31 23:59:59"),
-            "10:12:12")
+            "2021-01-07 23:59:59")
 
     );
   }
 
-  @ParameterizedTest
-  public void testTimestampadd(String unit, int interval, ExprValue datetimeExpr, String expected) {
-    FunctionExpression expr = DSL.timestampadd(
+  private static FunctionExpression timestampaddQuery(String unit, int amount, ExprValue datetimeExpr) {
+    return DSL.timestampadd(
         DSL.literal(unit),
-        DSL.literal(new ExprIntegerValue(interval)),
+        DSL.literal(new ExprIntegerValue(amount)),
         DSL.literal(datetimeExpr)
     );
+  }
 
+  @ParameterizedTest
+  public void testTimestampadd(String unit, int amount, ExprValue datetimeExpr, String expected) {
+    FunctionExpression expr = timestampaddQuery(unit, amount, datetimeExpr);
     assertEquals(new ExprDatetimeValue(expected), eval(expr));
   }
 
@@ -112,8 +117,40 @@ class TimeStampAddTest extends ExpressionTestBase {
   }
 
   //TODO: Add test to compare outputs when using a string/timestamp/datetime for 3rd argument.
+  @Test
+  public void testDifferentInputTypesHaveSameResult() {
+    String part = "SECOND";
+    int amount = 1;
+    FunctionExpression dateExpr = timestampaddQuery(part, amount, new ExprDateValue("2000-01-01"));
+    FunctionExpression stringExpr = timestampaddQuery(part, amount, new ExprStringValue("2000-01-01"));
+    FunctionExpression datetimeExpr = timestampaddQuery(
+        part,
+        amount,
+        new ExprDatetimeValue("2000-01-01 00:00:00"));
+    FunctionExpression timestampExpr = timestampaddQuery(
+        part,
+        amount,
+        new ExprTimestampValue("2000-01-01 00:00:00"));
 
-  //TODO: Add test for invalid arguments
+    assertAll(
+        () -> assertEquals(eval(dateExpr), eval(stringExpr)),
+        () -> assertEquals(eval(dateExpr), eval(datetimeExpr)),
+        () -> assertEquals(eval(dateExpr), eval(timestampExpr))
+    );
+  }
+
+  @Test
+  public void testInvalidArguments() {
+    FunctionExpression expr1 = timestampaddQuery("INVALID", 1, new ExprDateValue("2000-01-01"));
+    FunctionExpression expr2 = timestampaddQuery("WEEK", 1, new ExprStringValue("2000-13-01"));
+    FunctionExpression expr3 = timestampaddQuery("WEEK", 1, new ExprStringValue("2000-01-40"));
+
+    assertAll(
+        () -> assertThrows(SemanticCheckException.class, () -> eval(expr1)),
+        () -> assertThrows(SemanticCheckException.class, () -> eval(expr2)),
+        () -> assertThrows(SemanticCheckException.class, () -> eval(expr3))
+    );
+  }
 
   //TODO: Add test to check for null value return???
   private ExprValue eval(Expression expression) {
