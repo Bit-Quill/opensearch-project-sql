ddb09d4c91cd5f37c0b9fefdc42dd97771e7ab97
Guian Gumpac <guian.gumpac@improving.com>
2023-03-17 15:41:53 -0700

    Reverted OpenSearchIndexScan changes as it broke IT
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/storage/OpenSearchIndexScan.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/storage/OpenSearchIndexScan.java
@@ -83,7 +83,7 @@ public class OpenSearchIndexScan extends TableScanOperator {
     request = requestBuilder.build();
     iterator = Collections.emptyIterator();
     queryCount = 0;
-    fetchNextBatch();
+    rawResponse = fetchNextBatch().getRawResponse();
   }
 
   @Override
@@ -102,12 +102,12 @@ public class OpenSearchIndexScan extends TableScanOperator {
     return iterator.next();
   }
 
-  protected void fetchNextBatch() {
+  protected OpenSearchResponse fetchNextBatch() {
     OpenSearchResponse response = client.search(request);
     if (!response.isEmpty()) {
       iterator = response.iterator();
-      rawResponse = response.getRawResponse();
     }
+    return response;
   }
 
   @Override
