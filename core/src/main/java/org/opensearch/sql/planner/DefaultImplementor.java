a00e16c9da0065cb554af3e7a129f9861d80e74d
Guian Gumpac <guian.gumpac@improving.com>
2023-03-29 17:21:41 -0700

    Fixed bug
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>

--- core/src/main/java/org/opensearch/sql/planner/DefaultImplementor.java
+++ core/src/main/java/org/opensearch/sql/planner/DefaultImplementor.java
@@ -98,7 +98,7 @@ public class DefaultImplementor<C> extends LogicalPlanNodeVisitor<PhysicalPlan, 
 
   @Override
   public PhysicalPlan visitUnnest(LogicalNested node, C context) {
-    if (node.getFields().get(0).get("field").getAttr().equals("*")) {
+    if (node.getFields().stream().anyMatch(field -> field.get("field").getAttr().equals("*"))) {
       return new NestedOperator(visitChild(node,context), node.getFields(), node.getProjectList());
     }
     return new NestedOperator(visitChild(node, context), node.getFields());
