07e3988bd9102c094b44157984daa63138b1629f
Matthew Wells <matthew.wells@improving.com>
2023-05-17 14:19:42 -0700

    Fixed checkstyle failures
    Signed-off-by: Matthew Wells <matthew.wells@improving.com>

--- sql/src/main/java/org/opensearch/sql/sql/antlr/SQLSyntaxParser.java
+++ sql/src/main/java/org/opensearch/sql/sql/antlr/SQLSyntaxParser.java
@@ -7,11 +7,7 @@
 package org.opensearch.sql.sql.antlr;
 
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeListener;
-import org.antlr.v4.runtime.tree.TerminalNode;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.opensearch.sql.common.antlr.CaseInsensitiveCharStream;
@@ -20,22 +16,12 @@ import org.opensearch.sql.common.antlr.SyntaxAnalysisErrorListener;
 import org.opensearch.sql.sql.antlr.parser.OpenSearchSQLLexer;
 import org.opensearch.sql.sql.antlr.parser.OpenSearchSQLParser;
 
-import static org.opensearch.sql.sql.antlr.parser.OpenSearchSQLLexer.*;
-
 /**
  * SQL syntax parser which encapsulates an ANTLR parser.
  */
 public class SQLSyntaxParser implements Parser {
 
   private static final Logger LOG = LogManager.getLogger(SQLSyntaxParser.class);
-
-  public OpenSearchSQLParser createParser(String query, AnonymizerListener anonymizer) {
-    OpenSearchSQLLexer lexer = new OpenSearchSQLLexer(new CaseInsensitiveCharStream(query));
-    OpenSearchSQLParser parser = new OpenSearchSQLParser(new CommonTokenStream(lexer));
-    parser.addErrorListener(new SyntaxAnalysisErrorListener());
-    parser.addParseListener(anonymizer);
-    return parser;
-  }
 
   /**
    * Parse a SQL query by ANTLR parser.
@@ -45,8 +31,11 @@ public class SQLSyntaxParser implements Parser {
   @Override
   public ParseTree parse(String query) {
     AnonymizerListener anonymizer = new AnonymizerListener();
-    ParseTree parseTree = createParser(query, anonymizer).root();
+    OpenSearchSQLLexer lexer = new OpenSearchSQLLexer(new CaseInsensitiveCharStream(query));
+    OpenSearchSQLParser parser = new OpenSearchSQLParser(new CommonTokenStream(lexer));
+    parser.addErrorListener(new SyntaxAnalysisErrorListener());
+    parser.addParseListener(anonymizer);
     LOG.info("New Engine Request Query: {}", anonymizer.getAnonymizedQueryString());
-    return parseTree;
+    return parser.root();
   }
 }
