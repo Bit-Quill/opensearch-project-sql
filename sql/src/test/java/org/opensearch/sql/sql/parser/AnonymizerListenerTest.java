164a0ae3de4466b085a314deceddef22eb3e7154
Matthew Wells <matthew.wells@improving.com>
2023-05-19 11:38:50 -0700

    Fixed failing jacoco and checkstyle tests, and cleaned up code
    Signed-off-by: Matthew Wells <matthew.wells@improving.com>

--- sql/src/test/java/org/opensearch/sql/sql/parser/AnonymizerListenerTest.java
+++ sql/src/test/java/org/opensearch/sql/sql/parser/AnonymizerListenerTest.java
@@ -11,68 +11,66 @@ import org.junit.jupiter.api.Test;
 import org.opensearch.sql.sql.antlr.AnonymizerListener;
 
 public class AnonymizerListenerTest {
-    AnonymizerListener anonymizer = new AnonymizerListener();
+  AnonymizerListener anonymizer = new AnonymizerListener();
+  ErrorNode errorNode = new ErrorNode() {
 
-    ErrorNode errorNode = new ErrorNode () {
-
-        @Override
-        public Interval getSourceInterval() {
-            return null;
-        }
-
-        @Override
-        public ParseTree getParent() {
-            return null;
-        }
-
-        @Override
-        public Object getPayload() {
-            return null;
-        }
-
-        @Override
-        public ParseTree getChild(int i) {
-            return null;
-        }
-
-        @Override
-        public int getChildCount() {
-            return 0;
-        }
-
-        @Override
-        public String toStringTree() {
-            return null;
-        }
-
-        @Override
-        public void setParent(RuleContext parent) {
-
-        }
-
-        @Override
-        public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
-            return null;
-        }
-
-        @Override
-        public String getText() {
-            return null;
-        }
-
-        @Override
-        public String toStringTree(Parser parser) {
-            return null;
-        }
-
-        @Override
-        public Token getSymbol() {
-            return null;
-        }
-    };
-
-    @Test
-    public void visitErrorNodeTest() {
-        anonymizer.visitErrorNode(errorNode);
+    @Override
+    public Interval getSourceInterval() {
+      return null;
     }
+
+    @Override
+    public ParseTree getParent() {
+      return null;
+    }
+
+    @Override
+    public Object getPayload() {
+      return null;
+    }
+
+    @Override
+    public ParseTree getChild(int i) {
+      return null;
+    }
+
+    @Override
+    public int getChildCount() {
+      return 0;
+    }
+
+    @Override
+    public String toStringTree() {
+      return null;
+    }
+
+    @Override
+    public String toStringTree(Parser parser) {
+      return null;
+    }
+
+    @Override
+    public void setParent(RuleContext parent) {
+    }
+
+    @Override
+    public <T> T accept(ParseTreeVisitor<? extends T> visitor) {
+      return null;
+    }
+
+    @Override
+    public String getText() {
+      return null;
+    }
+
+    @Override
+    public Token getSymbol() {
+      return null;
+    }
+  };
+
+  @Test
+  public void visitErrorNodeTest() {
+    anonymizer.visitErrorNode(errorNode);
+  }
 }
