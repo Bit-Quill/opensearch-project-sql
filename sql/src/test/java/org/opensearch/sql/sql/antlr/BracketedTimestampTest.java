05b764873a5c6890da14c596b9ae1a9c59c05464
Matthew Wells <matthew.wells@improving.com>
2023-07-20 12:54:55 -0700

    fixed checkstyle errors
    Signed-off-by: Matthew Wells <matthew.wells@improving.com>

--- sql/src/test/java/org/opensearch/sql/sql/antlr/BracketedTimestampTest.java
+++ sql/src/test/java/org/opensearch/sql/sql/antlr/BracketedTimestampTest.java
@@ -3,63 +3,63 @@ package org.opensearch.sql.sql.antlr;
 import org.junit.jupiter.api.Test;
 
 public class BracketedTimestampTest extends SQLParserTest {
-    @Test
-    void d_test() {
-        acceptQuery("SELECT {d '2001-05-07'}");
-    }
+  @Test
+  void date_shortened_test() {
+    acceptQuery("SELECT {d '2001-05-07'}");
+  }
 
-    @Test
-    void date_test() {
-        acceptQuery("SELECT {date '2001-05-07'}");
-    }
+  @Test
+  void date_test() {
+    acceptQuery("SELECT {date '2001-05-07'}");
+  }
 
-    @Test
-    void t_test() {
-        acceptQuery("SELECT {t '10:11:12'}");
-    }
+  @Test
+  void time_shortened_test() {
+    acceptQuery("SELECT {t '10:11:12'}");
+  }
 
-    @Test
-    void time_test() {
-        acceptQuery("SELECT {time '10:11:12'}");
-    }
+  @Test
+  void time_test() {
+    acceptQuery("SELECT {time '10:11:12'}");
+  }
 
-    @Test
-    void ts_test() {
-        acceptQuery("SELECT {ts '2001-05-07 10:11:12'}");
-    }
+  @Test
+  void timestamp_shortened_test() {
+    acceptQuery("SELECT {ts '2001-05-07 10:11:12'}");
+  }
 
-    @Test
-    void timestamp_test() {
-        acceptQuery("SELECT {timestamp '2001-05-07 10:11:12'}");
-    }
+  @Test
+  void timestamp_test() {
+    acceptQuery("SELECT {timestamp '2001-05-07 10:11:12'}");
+  }
 
-    @Test
-    void invalid_time_test() {
-        rejectQuery("SELECT {time '2001-05-07'}");
-    }
+  @Test
+  void invalid_time_test() {
+    rejectQuery("SELECT {time '2001-05-07'}");
+  }
 
-    @Test
-    void invalid_t_test() {
-        rejectQuery("SELECT {t '2001-05-07'}");
-    }
+  @Test
+  void invalid_t_test() {
+    rejectQuery("SELECT {t '2001-05-07'}");
+  }
 
-    @Test
-    void invalid_date_test() {
-        rejectQuery("SELECT {date '10:11:12'}");
-    }
+  @Test
+  void invalid_date_test() {
+    rejectQuery("SELECT {date '10:11:12'}");
+  }
 
-    @Test
-    void invalid_d_test() {
-        rejectQuery("SELECT {d '10:11:12'}");
-    }
+  @Test
+  void invalid_d_test() {
+    rejectQuery("SELECT {d '10:11:12'}");
+  }
 
-    @Test
-    void invalid_timestamp_test() {
-        rejectQuery("SELECT {timestamp 'invalid'}");
-    }
+  @Test
+  void invalid_timestamp_test() {
+    rejectQuery("SELECT {timestamp 'invalid'}");
+  }
 
-    @Test
-    void invalid_ts_test() {
-        rejectQuery("SELECT {ts 'invalid'}");
-    }
+  @Test
+  void invalid_ts_test() {
+    rejectQuery("SELECT {ts 'invalid'}");
+  }
 }
