74dbf15de39445a1f08a90f7d0f238868c079fee
Guian Gumpac <guian.gumpac@improving.com>
2023-07-20 19:05:26 -0700

    Made GeoPointValue an ExprTupleValue to enable access to lat and lon
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>

--- opensearch/src/test/java/org/opensearch/sql/opensearch/data/value/OpenSearchExprGeoPointValueTest.java
+++ opensearch/src/test/java/org/opensearch/sql/opensearch/data/value/OpenSearchExprGeoPointValueTest.java
@@ -7,8 +7,6 @@
 package org.opensearch.sql.opensearch.data.value;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.opensearch.sql.opensearch.data.type.OpenSearchGeoPointType;
@@ -18,28 +16,7 @@ class OpenSearchExprGeoPointValueTest {
   private OpenSearchExprGeoPointValue geoPointValue = new OpenSearchExprGeoPointValue(1.0, 1.0);
 
   @Test
-  void value() {
-    assertEquals(new OpenSearchExprGeoPointValue.GeoPoint(1.0, 1.0), geoPointValue.value());
-  }
-
-  @Test
   void type() {
     assertEquals(OpenSearchGeoPointType.of(), geoPointValue.type());
-  }
-
-  @Test
-  void compare() {
-    assertEquals(0, geoPointValue.compareTo(new OpenSearchExprGeoPointValue(1.0, 1.0)));
-    assertEquals(geoPointValue, new OpenSearchExprGeoPointValue(1.0, 1.0));
-  }
-
-  @Test
-  void equal() {
-    assertTrue(geoPointValue.equal(new OpenSearchExprGeoPointValue(1.0, 1.0)));
-  }
-
-  @Test
-  void testHashCode() {
-    assertNotNull(geoPointValue.hashCode());
   }
 }
