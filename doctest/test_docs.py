72d8beb186a53ac6ee4d1cb93290ee1575a012e6
Yury-Fridlyand <yury.fridlyand@improving.com>
2023-07-25 14:15:26 -0700

    Fix doctest clean up.
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- doctest/test_docs.py
+++ doctest/test_docs.py
@@ -128,7 +128,7 @@ def set_up(test):
 
 def tear_down(test):
     # drop leftover tables after each test
-    test_data_client.indices.delete(index=[ACCOUNTS, EMPLOYEES, PEOPLE, ACCOUNT2, NYC_TAXI, BOOKS, APACHE, WILDCARD, NESTED], ignore_unavailable=True)
+    test_data_client.indices.delete(index=[ACCOUNTS, EMPLOYEES, PEOPLE, ACCOUNT2, NYC_TAXI, BOOKS, APACHE, WILDCARD, NESTED, GEOPOINT], ignore_unavailable=True)
 
 
 docsuite = partial(doctest.DocFileSuite,
