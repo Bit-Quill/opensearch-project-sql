47abe6e15cbb12cc5abd3d5c0eca0d26fba76b2d
Mitchell Gale <Mitchell.Gale@improving.com>
2023-08-08 13:47:10 -0700

    Fixing changed syntax to toStirng.
    Signed-off-by: Mitchell Gale <Mitchell.Gale@improving.com>

--- plugin/src/main/java/org/opensearch/sql/plugin/rest/RestQuerySettingsAction.java
+++ plugin/src/main/java/org/opensearch/sql/plugin/rest/RestQuerySettingsAction.java
@@ -19,7 +19,6 @@ import org.opensearch.client.Requests;
 import org.opensearch.client.node.NodeClient;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.common.xcontent.XContentFactory;
-import org.opensearch.core.common.Strings;
 import org.opensearch.core.xcontent.XContentBuilder;
 import org.opensearch.core.xcontent.XContentParser;
 import org.opensearch.rest.BaseRestHandler;
