ba5222f34a321cc932ada92d55756757113908e4
Guian Gumpac <guian.gumpac@improving.com>
2023-08-22 15:37:24 -0700

    Added test for score without boost
    Signed-off-by: Guian Gumpac <guian.gumpac@improving.com>

--- core/src/test/java/org/opensearch/sql/analysis/OpenSearchAnalyzerTest.java
+++ core/src/test/java/org/opensearch/sql/analysis/OpenSearchAnalyzerTest.java
@@ -73,6 +73,33 @@ class OpenSearchAnalyzerTest extends AnalyzerTestBase {
   }
 
   @Test
+  public void analyze_filter_visit_score_function_without_boost() {
+
+    // setup
+    OpenSearchFunction scoreFunction = new OpenSearchFunction(
+        new FunctionName("match_phrase_prefix"), List.of());
+    when(builtinFunctionRepository.compile(any(), any(), any())).thenReturn(scoreFunction);
+
+    UnresolvedPlan unresolvedPlan =
+        AstDSL.filter(
+            AstDSL.relation("schema"),
+            new ScoreFunction(
+                AstDSL.function(
+                    "match_phrase_prefix",
+                    AstDSL.unresolvedArg("field", stringLiteral("field_value1")),
+                    AstDSL.unresolvedArg("query", stringLiteral("search query"))),
+                AstDSL.doubleLiteral(1.0)));
+
+    // test
+    LogicalPlan logicalPlan = analyze(unresolvedPlan);
+    OpenSearchFunction relevanceQuery =
+        (OpenSearchFunction) ((LogicalFilter) logicalPlan).getCondition();
+
+    // verify
+    assertEquals(true, relevanceQuery.isScoreTracked());
+  }
+
+  @Test
   public void analyze_filter_visit_score_function_with_unsupported_boost_SemanticCheckException() {
 
     // setup
