8b97e646e2d7c8e3f422841862d2fd72cb03ace4
Andrew Carbonetto <andrew.carbonetto@improving.com>
2025-01-08 10:49:03 -0800

    PPL: Add json and cast to json functions
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- core/src/test/java/org/opensearch/sql/expression/json/JsonFunctionsTest.java
+++ core/src/test/java/org/opensearch/sql/expression/json/JsonFunctionsTest.java
@@ -6,44 +6,146 @@
 package org.opensearch.sql.expression.json;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.opensearch.sql.data.model.ExprValueUtils.LITERAL_FALSE;
+import static org.opensearch.sql.data.model.ExprValueUtils.LITERAL_NULL;
 import static org.opensearch.sql.data.model.ExprValueUtils.LITERAL_TRUE;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.opensearch.sql.data.model.ExprBooleanValue;
+import org.opensearch.sql.data.model.ExprCollectionValue;
+import org.opensearch.sql.data.model.ExprDoubleValue;
+import org.opensearch.sql.data.model.ExprIntegerValue;
+import org.opensearch.sql.data.model.ExprLongValue;
+import org.opensearch.sql.data.model.ExprNullValue;
+import org.opensearch.sql.data.model.ExprStringValue;
+import org.opensearch.sql.data.model.ExprTupleValue;
 import org.opensearch.sql.data.model.ExprValue;
 import org.opensearch.sql.data.model.ExprValueUtils;
+import org.opensearch.sql.exception.SemanticCheckException;
 import org.opensearch.sql.expression.DSL;
 import org.opensearch.sql.expression.FunctionExpression;
 
 @ExtendWith(MockitoExtension.class)
 public class JsonFunctionsTest {
-  private static final ExprValue JsonObject =
-      ExprValueUtils.stringValue("{\"a\":\"1\",\"b\":\"2\"}");
-  private static final ExprValue JsonArray = ExprValueUtils.stringValue("[1, 2, 3, 4]");
-  private static final ExprValue JsonScalarString = ExprValueUtils.stringValue("\"abc\"");
-  private static final ExprValue JsonEmptyString = ExprValueUtils.stringValue("");
-  private static final ExprValue JsonInvalidObject =
-      ExprValueUtils.stringValue("{\"invalid\":\"json\", \"string\"}");
-  private static final ExprValue JsonInvalidScalar = ExprValueUtils.stringValue("abc");
 
   @Test
   public void json_valid_returns_false() {
-    assertEquals(LITERAL_FALSE, execute(JsonInvalidObject));
-    assertEquals(LITERAL_FALSE, execute(JsonInvalidScalar));
+    final ExprValue JsonInvalidObject =
+        ExprValueUtils.stringValue("{\"invalid\":\"json\", \"string\"}");
+    assertEquals(LITERAL_FALSE, DSL.jsonValid(DSL.literal(JsonInvalidObject)).valueOf());
+
+    final ExprValue JsonInvalidScalar = ExprValueUtils.stringValue("abc");
+    assertEquals(LITERAL_FALSE, DSL.jsonValid(DSL.literal(JsonInvalidScalar)).valueOf());
   }
 
   @Test
   public void json_valid_returns_true() {
-    assertEquals(LITERAL_TRUE, execute(JsonObject));
-    assertEquals(LITERAL_TRUE, execute(JsonArray));
-    assertEquals(LITERAL_TRUE, execute(JsonScalarString));
-    assertEquals(LITERAL_TRUE, execute(JsonEmptyString));
+    final ExprValue JsonObject = ExprValueUtils.stringValue("{\"a\":\"1\",\"b\":\"2\"}");
+    assertEquals(LITERAL_TRUE, DSL.jsonValid(DSL.literal(JsonObject)).valueOf());
+
+    final ExprValue JsonArray = ExprValueUtils.stringValue("[1, 2, 3, 4]");
+    assertEquals(LITERAL_TRUE, DSL.jsonValid(DSL.literal(JsonArray)).valueOf());
+
+    final ExprValue JsonScalarString = ExprValueUtils.stringValue("\"abc\"");
+    assertEquals(LITERAL_TRUE, DSL.jsonValid(DSL.literal(JsonScalarString)).valueOf());
+
+    final ExprValue JsonEmptyString = ExprValueUtils.stringValue("");
+    assertEquals(LITERAL_TRUE, DSL.jsonValid(DSL.literal(JsonEmptyString)).valueOf());
   }
 
-  private ExprValue execute(ExprValue jsonString) {
-    FunctionExpression exp = DSL.jsonValid(DSL.literal(jsonString));
-    return exp.valueOf();
+  @Test
+  void json_returnsJsonObject() {
+    FunctionExpression exp;
+
+    // Setup
+    final String objectJson =
+        "{\"foo\": \"foo\", \"fuzz\": true, \"bar\": 1234, \"bar2\": 12.34, \"baz\": null, "
+            + "\"obj\": {\"internal\": \"value\"}, \"arr\": [\"string\", true, null]}";
+
+    LinkedHashMap<String, ExprValue> objectMap = new LinkedHashMap<>();
+    objectMap.put("foo", new ExprStringValue("foo"));
+    objectMap.put("fuzz", ExprBooleanValue.of(true));
+    objectMap.put("bar", new ExprLongValue(1234));
+    objectMap.put("bar2", new ExprDoubleValue(12.34));
+    objectMap.put("baz", ExprNullValue.of());
+    objectMap.put(
+        "obj", ExprTupleValue.fromExprValueMap(Map.of("internal", new ExprStringValue("value"))));
+    objectMap.put(
+        "arr",
+        new ExprCollectionValue(
+            List.of(new ExprStringValue("string"), ExprBooleanValue.of(true), ExprNullValue.of())));
+    ExprValue expectedTupleExpr = ExprTupleValue.fromExprValueMap(objectMap);
+
+    // exercise
+    exp = DSL.json_function(DSL.literal(objectJson));
+
+    // Verify
+    var value = exp.valueOf();
+    assertTrue(value instanceof ExprTupleValue);
+    assertEquals(expectedTupleExpr, value);
+  }
+
+  @Test
+  void json_returnsJsonArray() {
+    FunctionExpression exp;
+
+    // Setup
+    final String arrayJson = "[\"foo\", \"fuzz\", true, \"bar\", 1234, 12.34, null]";
+    ExprValue expectedArrayExpr =
+        new ExprCollectionValue(
+            List.of(
+                new ExprStringValue("foo"),
+                new ExprStringValue("fuzz"),
+                LITERAL_TRUE,
+                new ExprStringValue("bar"),
+                new ExprIntegerValue(1234),
+                new ExprDoubleValue(12.34),
+                LITERAL_NULL));
+
+    // exercise
+    exp = DSL.json_function(DSL.literal(arrayJson));
+
+    // Verify
+    var value = exp.valueOf();
+    assertTrue(value instanceof ExprCollectionValue);
+    assertEquals(expectedArrayExpr, value);
+  }
+
+  @Test
+  void json_returnsScalar() {
+    assertEquals(
+        new ExprStringValue("foobar"), DSL.json_function(DSL.literal("\"foobar\"")).valueOf());
+
+    assertEquals(new ExprIntegerValue(1234), DSL.json_function(DSL.literal("1234")).valueOf());
+
+    assertEquals(LITERAL_TRUE, DSL.json_function(DSL.literal("true")).valueOf());
+
+    assertEquals(LITERAL_NULL, DSL.json_function(DSL.literal("null")).valueOf());
+
+    assertEquals(LITERAL_NULL, DSL.json_function(DSL.literal("")).valueOf());
+
+    assertEquals(
+        ExprTupleValue.fromExprValueMap(Map.of()), DSL.json_function(DSL.literal("{}")).valueOf());
+  }
+
+  @Test
+  void json_returnsSemanticCheckException() {
+    // invalid type
+    assertThrows(
+        SemanticCheckException.class, () -> DSL.castJson(DSL.literal("invalid")).valueOf());
+
+    // missing bracket
+    assertThrows(SemanticCheckException.class, () -> DSL.castJson(DSL.literal("{{[}}")).valueOf());
+
+    // mnissing quote
+    assertThrows(
+        SemanticCheckException.class, () -> DSL.castJson(DSL.literal("\"missing quote")).valueOf());
   }
 }
--- integ-test/src/test/java/org/opensearch/sql/ppl/JsonFunctionsIT.java
+++ integ-test/src/test/java/org/opensearch/sql/ppl/JsonFunctionsIT.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
-public class JsonFunctionIT extends PPLIntegTestCase {
+public class JsonFunctionsIT extends PPLIntegTestCase {
   @Override
   public void init() throws IOException {
     loadIndex(Index.JSON_TEST);
@@ -48,6 +48,34 @@ public class JsonFunctionIT extends PPLIntegTestCase {
             String.format(
                 "source=%s | where not json_valid(json_string) | fields test_name",
                 TEST_INDEX_JSON_TEST));
+    verifySchema(result, schema("test_name", null, "string"));
+    verifyDataRows(result, rows("json invalid object"));
+  }
+
+  @Test
+  public void test_cast_json() throws IOException {
+    JSONObject result =
+        executeQuery(
+            String.format(
+                "source=%s | eval json=cast(json_string to json) | fields json",
+                TEST_INDEX_JSON_TEST));
+    verifySchema(result, schema("test_name", null, "string"));
+    verifyDataRows(
+        result,
+        rows("json object"),
+        rows("json array"),
+        rows("json scalar string"),
+        rows("json empty string"));
+  }
+
+  @Test
+  public void test_json() throws IOException {
+    JSONObject result;
+
+    result =
+        executeQuery(
+            String.format(
+                "source=%s | eval json=json(json_string) | fields json", TEST_INDEX_JSON_TEST));
     verifySchema(result, schema("test_name", null, "string"));
     verifyDataRows(result, rows("json invalid object"));
   }
